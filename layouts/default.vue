a6a7f255fd124eb0084c9f7d0253c7c9b7d0062b
maximfromit <bigmaxcapacity@gmail.com>
2024-12-22 00:09:36 +0300

    terminal - layouts/default - add default layout with navigation and footer components

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useRouter, useRoute } from "vue-router"
+
+const router = useRouter()
+const route = useRoute()
+
+function goBack() {
+  router.back()
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-12 h-dvh">
+    <UCard
+      class="sm:col-start-3 md:col-start-4 lg:col-start-5 col-span-12 sm:col-span-8 md:col-span-6 lg:col-span-4 grid grid-rows-[auto_1fr_auto]"
+    >
+      <template #header>
+        <div class="grid grid-cols-[40px_1fr_40px] items-center content-center">
+          <div v-if="route.path !== '/'">
+            <UButton
+              icon="i-lucide-arrow-left"
+              variant="ghost"
+              @click="goBack"
+            />
+          </div>
+          <div class="text-center text-3xl col-start-2">Ваши заметки</div>
+        </div>
+      </template>
+
+      <slot />
+
+      <template #footer>
+        <div class="text-end">2024</div>
+      </template>
+    </UCard>
+  </div>
+</template>
+
+<style>
+@import "tailwindcss";
+@import "@nuxt/ui";
+</style>
