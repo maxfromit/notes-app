68afb639629d51fb3b94941af11cf45986ccd9ca
maximfromit <bigmaxcapacity@gmail.com>
2024-12-22 00:10:41 +0300

    edit add note editing functionality with save and cancel options

---
+++ pages/edit.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { Note } from "../pages/index.vue"
+
+const route = useRoute()
+const router = useRouter()
+
+const note = ref<Note>({})
+
+console.log("route.query.id", route?.query?.id)
+
+function saveNote() {
+  // Save note logic
+  router.push("/")
+}
+
+function cancelEdit() {
+  if (confirm("Are you sure you want to cancel editing?")) {
+    router.push("/")
+  }
+}
+</script>
+
+<template>
+  <div>
+    <NoteEditor :note="note" @save="saveNote" @cancel="cancelEdit" />
+  </div>
+</template>
