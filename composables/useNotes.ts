2f80216a8ed8186372dff7a7356b5fc7c2941ead
maximfromit <bigmaxcapacity@gmail.com>
2024-12-23 11:57:44 +0300

    composables/useNotes add note management functionality with load, save, add, update, and delete operations

---
+++ composables/useNotes.ts
@@ -0,0 +1,53 @@
+import type { Note } from "../pages/index.vue"
+import l from "lodash"
+import { getNextId } from "@/utils/getNextId"
+
+export const useNotes = () => {
+  const notes = ref<Note[]>([])
+
+  const loadNotes = () => {
+    const storedNotes = localStorage.getItem("notes")
+    notes.value = storedNotes ? JSON.parse(storedNotes) : []
+  }
+
+  const saveNotes = (newNotes: Note[]) => {
+    notes.value = newNotes
+    localStorage.setItem("notes", JSON.stringify(newNotes))
+  }
+
+  const getNoteById = (id: number) => {
+    return notes.value.find((note) => note.id === id) || null
+  }
+
+  const addNote = (newNote: Note) => {
+    console.log("newNote", newNote)
+    newNote.id = getNextId(l.map(notes.value, (note) => note.id))
+    console.log("notes. ", notes.value)
+
+    notes.value.push(newNote)
+    saveNotes(notes.value)
+  }
+
+  const updateNote = (updatedNote: Note) => {
+    const existingNoteIndex = l.findIndex(notes.value, { id: updatedNote.id })
+    if (existingNoteIndex !== -1) {
+      notes.value[existingNoteIndex] = updatedNote
+      saveNotes(notes.value)
+    }
+  }
+
+  const deleteNoteById = (id: number) => {
+    notes.value = notes.value.filter((note) => note.id !== id)
+    saveNotes(notes.value)
+  }
+
+  return {
+    notes,
+    loadNotes,
+    saveNotes,
+    addNote,
+    updateNote,
+    getNoteById,
+    deleteNoteById,
+  }
+}
