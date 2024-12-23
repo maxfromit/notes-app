b04304e9c38c80ccee0c91e2bddbcf6946226247
maximfromit <bigmaxcapacity@gmail.com>
2024-12-23 16:00:41 +0300

    pages/[...all] add 404 error page template

---
+++ pages/[...all].vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="text-center">
+      <div style="font-size: 20vh">404</div>
+
+      <div class="text-h2" style="opacity: 0.4">Такой страницы нет...</div>
+    </div>
+  </div>
+</template>
