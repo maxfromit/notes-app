2480be91fa2a764847fceb4b3d75ebb6719451a4
maximfromit <bigmaxcapacity@gmail.com>
2024-12-23 13:53:13 +0300

    pages/index update loading state management and improve note sorting

--- pages/index.vue
+++ pages/index.vue
@@ -13,15 +13,22 @@ export type Note = {
 
 const { notes, loadNotes, deleteNoteById } = useNotes()
 
-onMounted(() => {
-  loadNotes()
+const loading = ref(true)
+
+onMounted(async () => {
+  await loadNotes()
+  loading.value = false
 })
 
 const sortedNotes = computed(() =>
-  notes.value.map((note) => ({
-    ...note,
-    todos: l.orderBy(note.todos, ["done"], ["desc"]),
-  }))
+  l.orderBy(
+    notes.value.map((note) => ({
+      ...note,
+      todos: l.orderBy(note.todos, "done"),
+    })),
+    "id",
+    "desc"
+  )
 )
 
 const router = useRouter()
@@ -36,12 +43,37 @@ function editNote(noteId: number) {
 
 function deleteNote(noteId: number) {
   deleteNoteById(noteId)
+  showSuccessToast({ title: "Заметка удалена" })
 }
 </script>
 
 <template>
-  {{ notes }}
+  <div v-if="loading" class="grid gap-10">
+    <div class="grid grid-cols-[1fr_auto]">
+      <div class="text-xl">
+        <USkeleton class="h-6 w-32" />
+      </div>
+      <USkeleton class="h-6 w-6" />
+    </div>
+    <div class="grid gap-5">
+      <UCard>
+        <div class="grid grid-cols-[1fr_auto]">
+          <USkeleton class="h-4 w-48" />
+          <div class="grid grid-cols-2 gap-1">
+            <USkeleton class="h-3 w-3" />
+            <USkeleton class="h-3 w-3" />
+          </div>
+        </div>
+        <div class="text-sm">
+          <USkeleton class="h-4 w-2/3" />
+          <USkeleton class="h-4 w-2/3" />
+          <USkeleton class="h-4 w-1/3" />
+        </div>
+      </UCard>
+    </div>
+  </div>
   <NoteList
+    v-else
     :notes="sortedNotes"
     @edit="editNote"
     @delete="deleteNote"
