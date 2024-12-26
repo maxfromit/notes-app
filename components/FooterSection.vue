d4874bf8ce1cce5bf0e2c7e338872aa3570742cc
maximfromit <bigmaxcapacity@gmail.com>
2024-12-27 02:00:19 +0300

    components/FooterSection show only my nickname  in footer

--- components/FooterSection.vue
+++ components/FooterSection.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import dayjs from "dayjs"
-import "dayjs/locale/ru"
+// import dayjs from "dayjs"
+// import "dayjs/locale/ru"
 
-dayjs.locale("ru")
+// dayjs.locale("ru")
 
-function getCurrentDate() {
-  return dayjs().format("D MMMM YYYY")
-}
+// function getCurrentDate() {
+//   return dayjs().format("D MMMM YYYY")
+// }
 </script>
 
 <template>
-  <div class="text-end">{{ getCurrentDate() }}</div>
+  <!-- <div class="text-end">{{ getCurrentDate() }}</div> -->
+
+  <div class="text-end">maxfromit</div>
 </template>
